1aaae7328269a8a3cf046450858bb1b82f629217
Takuto Oshima <takuto.oshima@tohmatsu.co.jp>
2025-12-28 20:24:43 +0900

    move cors

--- amazon/cors-test/terraform/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "front" {
-  name                 = "${var.project}-front"
-  image_tag_mutability = "MUTABLE"
-}
-
-resource "aws_ecr_repository" "back" {
-  name                 = "${var.project}-back"
-  image_tag_mutability = "MUTABLE"
-}
